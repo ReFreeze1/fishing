85669d4b01a86109434e2a7d431d3687f8d45894
ReFreeze1 <81642777+ReFreeze1@users.noreply.github.com>
2021-06-06 13:00:17 +0200

    ready

--- fishing.cpp
+++ fishing.cpp
@@ -6,5 +6,29 @@
 
 int main()
 {
-   
+	std::ifstream river("river.txt");
+	std::ofstream bascket("bascket.txt", std::ios::app);
+	std::string fish_kind;
+	std::string buffer;
+	int count = 0;
+	char ans;
+	do {
+		std::cout << "What kind of fish you fish? ";
+		std::cin >> fish_kind;
+
+		while (!river.eof()) {
+			river >> buffer;
+			if (fish_kind == buffer) {
+				bascket << buffer << std::endl;
+				count++;
+			}
+		}
+		river.seekg(0);
+		std::cout << "One more? (y/n): ";
+		std::cin >> ans;
+	} while (ans=='y');
+	std::cout << "You cath: " << count << " fish.";
+	
+	bascket.close();
+	river.close();
 }
